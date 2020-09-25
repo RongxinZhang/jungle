512d21fffb679ae524229a6aa34e7b5c8a3b6a86
Rongxin Zhang <mrrongxin@gmail.com>
2020-09-24 22:37:33 -0700

    Out of stock

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,15 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # puts @products.length
+    @products.each do |p|
+      puts p.quantity
+    end
+    # return @products
   end
 
-  def show
-    @product = Product.find params[:id]
-  end
+  # def show
+  #   @product = Product.find params[:id]
+  # end
 
 end
