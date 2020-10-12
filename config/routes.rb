554990895f0088568a1b3800b804e2a3d39b750d
Rongxin <mrrongxin@gmail.com>
2020-10-12 23:43:12 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   get '/signup' => 'users#new'
-    post '/users' => 'users#create'
+  post '/users' => 'users#create'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
